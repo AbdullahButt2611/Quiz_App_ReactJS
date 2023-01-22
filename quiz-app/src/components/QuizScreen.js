c2ac72f5c06aa06b3ec913dfbd98bdd9957652b2
M. Abdullah Butt <81871162+AbdullahButt2611@users.noreply.github.com>
2023-01-22 23:31:56 +0500

    Designing...

--- quiz-app/src/components/QuizScreen.js
+++ quiz-app/src/components/QuizScreen.js
@@ -4,31 +4,50 @@ import QuestionList from "../data/questions.json";
 import Question from "./Question.js"
 import QuizResult from "./QuizResult.js"
 
-function QuizScreen(){
+function QuizScreen({retry}){
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [markedAnswers, setMarkedAnswers] = useState(new Array(QuestionList.length));
     const isQuestionEnd = currentQuestionIndex === QuestionList.length;
 
+    function calculateResult(){
+        let correct  = 0;
+        QuestionList.forEach((question, index)=>{
+            if(question.correctOptionIndex == markedAnswers[index]){
+                correct++;
+            }
+        });
+        return{
+            total:QuestionList.length,
+            correct:correct,
+            percentage:Math.trunc((correct / QuestionList.length) * 100)
+        };
+    }
+
     return (
         <div className="quiz-screen">
-            isQuestionEnd ? (
-                <QuizResult />
-            ) : (
-                <Question 
-                    question = {QuestionList[currentQuestionIndex]}
-                    totalQuestions = {QuestionList.length}
-                    currentQuestion = {currentQuestionIndex+1}
-                    setAnswer = { (index) => {
-                        setMarkedAnswers((arr) => {
-                            let newArr = [...arr];
-                            newArr[currentQuestionIndex] = index;
-                            return newArr;
-                        });
+            {
+                isQuestionEnd ? (
+                    <QuizResult 
+                        result={calculateResult()}
+                        retry = {retry}
+                    />
+                ) : (
+                    <Question 
+                        question = {QuestionList[currentQuestionIndex]}
+                        totalQuestions = {QuestionList.length}
+                        currentQuestion = {currentQuestionIndex+1}
+                        setAnswer = { (index) => {
+                            setMarkedAnswers((arr) => {
+                                let newArr = [...arr];
+                                newArr[currentQuestionIndex - 1] = index;
+                                return newArr;
+                            });
 
-                        setCurrentQuestionIndex(currentQuestionIndex + 1);
-                    } }
-                />
-            )
+                            setCurrentQuestionIndex(currentQuestionIndex + 1);
+                        } }
+                    />
+                )
+            }
         </div>
     );
 }
